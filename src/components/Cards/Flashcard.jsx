e336b685f47c5cec1892fa16caf3a0d2fc4abcbf
sarahpicard <sarahannrey225@gmail.com>
2022-05-02 17:14:24 -0400

    begin state for current flashcard numbers

--- src/components/Cards/Flashcard.jsx
+++ src/components/Cards/Flashcard.jsx
@@ -1,8 +1,25 @@
+import e from "express"
+import { useState } from "react"
+
 const Flashcard = ({ random, random2, numbers }) => {
+  const [num1, setNum1] = useState('')
+  const [num2, setNum2] = useState('')
+
+  const handleNum1 = () => {
+    e.preventDefault()
+    setNum1(Math.floor(Math.random() * (numbers.length)))
+  }
+  const handleNum2 = () => {
+    e.preventDefault()
+    setNum2(Math.floor(Math.random() * (numbers.length)))
+  }
+
   return (
     <div className="container mx-auto bg-orange-200 h-96 w-1/5 mt-12 rounded-2xl shadow-lg">
-      <p>{numbers[random]}</p>
-      <p>{numbers[random2]}</p>
+      <p>{num1}</p>
+      <p>+</p>
+      <p>{num2}</p>
+      <p></p>
     </div>
   )
 }
